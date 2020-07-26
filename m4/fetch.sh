3dff5922fbb6d32557b78bbc8dd089d434039e4d
Bert van der Weerd <bert@stanzabird.nl>
2020-07-27 00:41:23 +0200

    improved the m4 macro dir with a fetch.sh command

---
+++ m4/fetch.sh
@@ -0,0 +1,15 @@
+#
+# GNU Autoconf Macro Archive.
+#
+# http://www.gnu.org/software/autoconf-archive/The-Macros.html
+#
+
+wget --output-document=ax_cxx_compile_stdcxx.m4     'http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_cxx_compile_stdcxx.m4'
+wget --output-document=ax_cxx_compile_stdcxx_17.m4  'http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_cxx_compile_stdcxx_17.m4'
+
+wget --output-document=ax_prog_flex.m4              'http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_prog_flex.m4'
+wget --output-document=ax_prog_bison.m4             'http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_prog_bison.m4'
+
+#wget --output-document= ''
+#wget --output-document= ''
+#wget --output-document= ''
